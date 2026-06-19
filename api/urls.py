8b358e8f5aac9a633f3188512a745c20fb071661
Glenferdinza <glenferdinza21@gmail.com>
2026-06-19 11:16:09 +0700

    Support generate routes in Django

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('itinerary/', views.get_itinerary, name='get_itinerary'),
+    path('generate/', views.get_itinerary, name='get_itinerary_generate_slash'),
+    path('generate', views.get_itinerary, name='get_itinerary_generate'),
 ]
